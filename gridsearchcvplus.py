e9ac84ab57d8de2a0561d13d59a22fd01c9b964b
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-09-16 21:27:01 +0200

    GridSearchCVPlus Update

--- gridsearchcvplus.py
+++ gridsearchcvplus.py
@@ -1,23 +1,24 @@
 from sklearn.tree import DecisionTreeClassifier
-
 from sklearn.model_selection import KFold
 from itertools import product
 
 
-class TreeCalculation():
+
+class ModelCalculations():
+    """
+    Class that performs custom calculations from a model's attributes.
+    For example, calculates the number of leaf nodes of a DecisionTreeClassifier
+    """
     
     def __init__(self):
-        self.tree = None
-        self.calcs_names = []
-        
-        self._get_calcs_names()
-        
-    def set_tree(self, tree):
-        self.tree = tree
-        
-    def _get_calcs_names(self):
-        if not self.calcs_names:
-            self.calculate()
+        """Initialize ModelCalculations instance"""
+        self.estimator = None
+              
+            
+    def set_estimator(self, estimator):
+        """Sets the estimator to do the calculations for"""
+        self.estimator = estimator
+            
     
     def get_leaves_count(self):
         """
@@ -25,101 +26,128 @@ class TreeCalculation():
         The child_left[i] method returns the number of the node the is the left child of node i.
         If the node i is a leaf node, child_left[i] = -1. Therefore
         """
-        if self.tree is None:
-            self.calcs_names.append("leaves_count")
-        else:
-            t = self.tree.tree_
-            n = t.node_count
-            leaves = len([i for i in range(n) if t.children_left[i]== -1])
-            return leaves
+        t = self.estimator.tree_
+        n = t.node_count
+        leaves = len([i for i in range(n) if t.children_left[i]== -1])
+        return {"leaves_count": leaves}
+    
     
     def calculate(self):
-        return [self.get_leaves_count()]
-     
+        """ Entry point to do all the calculations"""
+        
+        return {**self.get_leaves_count()}
 
-class TreeGridSearchCV():
+
+    
+    
+class GridSearchCVPlus():
+    """
+    Performs a grid search cross validation. It can perform custom calculations for a model from its
+    attributes, by passing a ModelCalculation instance in model_calculations
+    """
     
     def __init__(self, estimator, param_grid, scoring='roc_auc', cv=3, shuffle=True,
-                 refit=True, return_train_score=True, tree_calculation=None):
-        self.model = estimator
-        self.param_grid = param_grid
+                 refit=True, return_train_score=True, model_calculations=None):
+        """Initialisation of grid search cross validation configuration, and parameters"""
         
+        # GridSeachCV configuration
+        self.estimator = estimator
+        self.param_grid = param_grid
         self.n_splits = cv
         self.shuffle = shuffle
-        
         self.refit = refit
         self.return_train_score = return_train_score
+        self.random_state = self.estimator.random_state
         
-        self.tree_calculation = tree_calculation
+        # Custom calculations
+        self.model_calculations = model_calculations
         
+        # Cross-validation results
+        self.cv_results_ = {}
+        
+        # Best parameters, estimator and score
         self.best_params_ = None
+        self.best_calculations_ = None
         self.best_estimator_ = None
         self.best_score_ = None
         
+    def fit(self, features, target):        
+        """ Fit the grid search cross validation instance to the dataset """
+        
+        # Initialise cross validation results dictionary
+        self.cv_results_["params"] = []      
+        for split in range(self.n_splits):
+            self.cv_results_[f"split{split}_train_score"] = []
+        for split in range(self.n_splits):
+            self.cv_results_[f"split{split}_test_score"] = []
+        if self.model_calculations is not None:
+            self.cv_results_["model_calculations"] = []
+            
+        # Perform cross validation
+        self._grid_search_cross_validate(features, target)
+        
+        # Look for best test/validation score
+        test_scores = np.vstack([self.cv_results_[f"split{i}_test_score"] for i in range(self.n_splits)])
+        mean_test_scores = np.mean(test_scores, axis=0)
+        best_score_arg = np.argmax(mean_test_scores)
+        
+        # Save best validation score, parameters, and calculations
+        self.best_score_ = mean_test_scores[best_score_arg]
+        self.best_params_ = self.cv_results_["params"][best_score_arg]
+        self.best_calculations_ = self.cv_results_["model_calculations"][best_score_arg]
+        
+        # Fit best estimator to the dataset
+        self.best_estimator_ = self.estimator
+        self.best_estimator_.set_params(**self.best_params_)
+        features_train, features_test, target_train, target_test = train_test_split(features, target,
+                                                                                    test_size=0.4,
+                                                                                    random_state=self.random_state)
+        self.best_estimator_.fit(features_train, target_train)
+        
+        
                 
-    def fit(self, features, target):
-        
-        params_scores_calcs = []
-        
+    def _grid_search_cross_validate(self, features, target):
+        """ 
+        Cross validation, save parameters, training and testing scores for each split,
+        and custom calculations 
+        """        
         # Find all combinations of parameters
         param_combinations = list(product(*self.param_grid.values()))
         for params in param_combinations:
             
-            # Define a tree with each combination of parameters
+            # Set the parameters of the estimator each combination of parameters, and store them
             params_dict = dict(zip(self.param_grid.keys(), params))
-            tree = DecisionTreeClassifier(random_state=42)
-            tree.set_params(**params_dict)
+            self.cv_results_["params"].append(params_dict)
+            self.estimator.set_params(**params_dict)
 
             # Cross-validate
-            kf = KFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=42)
-            train_scores, val_scores, quantities = [], [], []
+            kf = KFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=self.random_state)
+            train_scores, val_scores, calculations = [], [], []
+            split_number = 0
             for train_idx, test_idx in kf.split(features):
+                
                 # Fit the tree to the training data
                 features_train, target_train,  = features.iloc[train_idx], target.iloc[train_idx]
                 features_test, target_test = features.iloc[test_idx], target.iloc[test_idx]
-                tree.fit(X=features_train, y=target_train)
+                self.estimator.fit(X=features_train, y=target_train)
                 
                 # Score the train dataset and testing dataset (validation)
-                train_pred = tree.predict_proba(X=features_train)[:, 1]
+                train_pred = self.estimator.predict_proba(X=features_train)[:, 1]
                 train_score = roc_auc_score(y_score=train_pred, y_true=target_train)
                 train_scores.append(train_score)
-                
-                test_pred = tree.predict_proba(X=features_test)[:, 1]
+                test_pred = self.estimator.predict_proba(X=features_test)[:, 1]
                 val_score = roc_auc_score(y_score=test_pred, y_true=target_test)
                 val_scores.append(val_score)
+                self.cv_results_[f"split{split_number}_train_score"].append(train_score)
+                self.cv_results_[f"split{split_number}_test_score"].append(val_score)
                 
-                
-#                 # Calculate the extra quantities
-#                 if self.tree_calculation is not None:
-#                     self.tree_calculation.set_tree(tree)
-#                     self.tree_calculation.calculate()
-#                     quantities.append()
+                # Calculate the custom model quantities
+                if self.model_calculations is not None:
+                    self.model_calculations.set_estimator(self.estimator)   # Set the current tree for tree calculations
+                    calcs = self.model_calculations.calculate()             # Get tree calculations
+                    self.cv_results_["model_calculations"].append(calcs)    # Aggregate calculations
             
-            # Keep calculations for median validation score
-            cv_scores = np.transpose(np.array([train_scores, val_scores]))
-            median_val_row = cv_scores[np.where(cv_scores[:, 1] == np.median(cv_scores[:, 1]))][0]
-            median_val_row = np.concatenate((list(params_dict.values()), median_val_row))
-            params_scores_calcs.append(median_val_row)
-            
-        # Get columns
-        hyperparams_names = list(self.param_grid.keys())
-        cols = hyperparams_names + ['train_score', 'val_score']
-        if self.tree_calculation is not None:
-            cols += self.tree_calculation.calcs_names
-        
-        # Stack the tree features for each hyperparameter combination vertically
-        params_scores_calcs = np.vstack(params_scores_calcs)
-        print(params_scores_calcs[:,-1])
-        # Find the best hyperparameters that lead to best validation score
-        self.best_score_ = np.max(params_scores_calcs[:,-1])
-        best_params_scores_calcs = params_scores_calcs[np.where(params_scores_calcs[:,-1] == self.best_score_)]
-        self.best_params_ = dict(zip(hyperparams_names, best_params_scores_calcs[0,: len(hyperparams_names)]))
-        # Define and train the estimator with the best hyperparameters
-        self.best_estimator = None
-        
-        # Return a DataFrame
-        params_scores_calcs = dict(zip(cols, np.transpose(params_scores_calcs)))
-        return pd.DataFrame(params_scores_calcs)
+                split_number += 1
 
             
 
