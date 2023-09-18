32828df560d170479b8b7d29da3d8548a107eb9e
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-09-19 00:08:51 +0200

    Addition of run_cross_validation

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+
 from sklearn.tree import _tree
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import cross_validate
+
+
 
 def tree_to_code(tree, feature_names):
     if isinstance(tree, DecisionTreeClassifier):
@@ -30,3 +39,65 @@ def tree_to_code(tree, feature_names):
             val = pred[1]/sum(pred) if model == 'clf' else pred[0]
             print(indent, f'return {val:.2%}')
     recurse(0, 1)
+    
+    
+def stack_cv_results(scores, metrics):
+    """ Stack the results from cross validation """
+    
+    columns = pd.MultiIndex.from_tuples(
+        [tuple(m.split('_', 1)) for m in scores.keys()],
+        names=['Dataset', 'Metric'])
+    
+    data = np.array(list(scores.values())).T                          # Scores for each metric
+    df = pd.DataFrame(data=data, columns=columns).iloc[:, 2:]         # Drop the fit and score times
+    
+    results = pd.melt(df, value_name='Value')                         # Melt dataframe for Value
+    results.Metric = results.Metric.apply(lambda x: metrics.get(x))   # Get titles for metrics
+    results.Dataset = results.Dataset.str.capitalize()                # Capitalise train and test
+    
+    return results
+
+
+def plot_cv_results(df, metrics, model=None):
+    """ Plot the score from the cross validation"""
+    m = list(metrics.values())
+    g = sns.catplot(data=df,
+                    x='Dataset', 
+                    y='Value', 
+                    hue='Dataset', 
+                    col='Metric',
+                    col_order=m,
+                    order=['Train', 'Test'],
+                    kind="box", 
+                    col_wrap=3,
+                    sharey=False,
+                    height=4, aspect=1.2)
+    
+    df = df.groupby(['Metric', 'Dataset']).Value.mean().unstack().loc[m]
+    
+    for i, ax in enumerate(g.axes.flat):
+        s = f"Train: {df.loc[m[i], 'Train']:>7.4f}\nTest:  {df.loc[m[i], 'Test'] :>7.4f}"
+        ax.text(0.75, 0.85, s, fontsize=10, transform=ax.transAxes, 
+                bbox=dict(facecolor='white', edgecolor='grey', boxstyle='round,pad=0.5'))
+    g.fig.suptitle(model, fontsize=16)
+    g.fig.subplots_adjust(top=.9)
+    
+
+def run_cross_validation(estimator, X, y, metrics, cv=3, model=None):
+    """"""
+    scores = cross_validate(estimator=estimator,
+                            X=X, 
+                            y=np.ravel(y),
+                            scoring=list(metrics.keys()),
+                            cv=cv,
+                            return_train_score=True,
+                            n_jobs=-1,
+                            verbose=1)
+    
+    results = stack_cv_results(scores, metrics)
+    plot_cv_results(results, metrics, model)
+    
+    return results
+    
+    
+    
